094d5a9132f6764a4a73319573387a9c8d94565c
Sau <kazi783+git@gmail.com>
2023-08-07 14:26:56 -0400

    added persona class

---
+++ Persona.py
@@ -0,0 +1,28 @@
+class Persona:
+    
+    def __init__(self):
+        self.persona_name = ''
+        self.persona_descrition = ''
+        self.characteristics = dict()
+
+    def readPersona(file):
+        pass
+
+    def displayPersona(self):
+        pass
+
+class Person
+    def __init__(self):
+        self.name = ""
+        self.fromState = ""
+        self.race = ""
+        self.religion = ""
+        self.gender = ""
+        self.age = 0
+        self.interest = []
+
+    def readPersonData(file):
+        pass
+
+    def displayPersonSummary():
+        pass
