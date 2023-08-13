c411226c6b9e66eb48deb43be3c066703336125b
Sau <kazi783+git@gmail.com>
2023-08-12 23:10:18 -0400

    api key removed

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ PATH_VHS = 'VHS/'
 FILE_VHS = 'covid-Lewandowsky13.csv'
 PATH_DATA = 'Data/'
 FILE_DATA = 'covid-Lewandowsky13.dat'
-API_KEY2 = "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"
+API_KEY2 = <your-api-key>
